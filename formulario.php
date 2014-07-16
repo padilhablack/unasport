29612aed49f6d0ed1cdac0ecb8be4d978c07f092
padilhablack <padilhamidia@hotmail.com>
2014-07-16 11:03:49 -0300

    primeiro commit

---
+++ formulario.php
@@ -0,0 +1,136 @@
+﻿<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Documento sem título</title>
+</head>
+
+<body>
+<form id="form1" name="form1" method="post" action="">
+  <table width="714" border="0" cellspacing="0" cellpadding="0">
+    <tr>
+      <td width="183">Nome Completo:</td>
+      <td width="10">&nbsp;</td>
+      <td width="521"><label for="textfield"></label>
+      <input type="text" name="textfield" id="textfield" /></td>
+    </tr>
+    <tr>
+      <td>RG:</td>
+      <td>&nbsp;</td>
+      <td><label for="textfield2"></label>
+      <input type="text" name="textfield2" id="textfield2" /></td>
+    </tr>
+    <tr>
+      <td>Data Nascimento:</td>
+      <td>&nbsp;</td>
+      <td><input type="text" name="textfield3" id="textfield3" /></td>
+    </tr>
+    <tr>
+      <td>Email:</td>
+      <td>&nbsp;</td>
+      <td><input type="text" name="textfield4" id="textfield4" /></td>
+    </tr>
+    <tr>
+      <td>&nbsp;</td>
+      <td>&nbsp;</td>
+      <td>&nbsp;</td>
+    </tr>
+    <tr>
+      <td>Endereço:</td>
+      <td>&nbsp;</td>
+      <td><input type="text" name="textfield5" id="textfield5" /></td>
+    </tr>
+    <tr>
+      <td>Rua:</td>
+      <td>&nbsp;</td>
+      <td><input type="text" name="textfield6" id="textfield6" /></td>
+    </tr>
+    <tr>
+      <td>Cidade:</td>
+      <td>&nbsp;</td>
+      <td><input type="text" name="textfield7" id="textfield7" /></td>
+    </tr>
+    <tr>
+      <td>CEP:</td>
+      <td>&nbsp;</td>
+      <td><input type="text" name="textfield8" id="textfield8" /></td>
+    </tr>
+    <tr>
+      <td>&nbsp;</td>
+      <td>&nbsp;</td>
+      <td>&nbsp;</td>
+    </tr>
+    <tr>
+      <td>Participante:</td>
+      <td>&nbsp;</td>
+      <td>&nbsp;</td>
+    </tr>
+    <tr>
+      <td align="right"><input type="radio" name="radio" id="radio" value="radio" />
+      <label for="radio"></label>        <label for="checkbox"></label></td>
+      <td>&nbsp;</td>
+      <td>Aluno UNASP interno</td>
+    </tr>
+    <tr>
+      <td align="right"><input type="radio" name="radio" id="radio2" value="radio" /></td>
+      <td>&nbsp;</td>
+      <td>Aluno UNASP não Interno</td>
+    </tr>
+    <tr>
+      <td align="right"><input type="radio" name="radio" id="radio3" value="radio" /></td>
+      <td>&nbsp;</td>
+      <td>Funcionário UNASP</td>
+    </tr>
+    <tr>
+      <td align="right"><input type="radio" name="radio" id="radio4" value="radio" /></td>
+      <td>&nbsp;</td>
+      <td>Membro da comunidade</td>
+    </tr>
+    <tr>
+      <td>&nbsp;</td>
+      <td>&nbsp;</td>
+      <td>&nbsp;</td>
+    </tr>
+    <tr>
+      <td>Modalidade:</td>
+      <td>&nbsp;</td>
+      <td>&nbsp;</td>
+    </tr>
+    <tr>
+      <td align="right"><input type="radio" name="radio" id="radio5" value="radio" /></td>
+      <td>&nbsp;</td>
+      <td>Caminhada (12km)</td>
+    </tr>
+    <tr>
+      <td align="right"><input type="radio" name="radio" id="radio6" value="radio" /></td>
+      <td>&nbsp;</td>
+      <td>Corrida (12km)</td>
+    </tr>
+    <tr>
+      <td align="right"><input type="radio" name="radio" id="radio7" value="radio" /></td>
+      <td>&nbsp;</td>
+      <td>Bicicleta (24km)</td>
+    </tr>
+    <tr>
+      <td>&nbsp;</td>
+      <td>&nbsp;</td>
+      <td>&nbsp;</td>
+    </tr>
+    <tr>
+      <td colspan="3">Ao final do evento será disponibilizado retorno de ônibus para o UNASP</td>
+    </tr>
+    <tr>
+      <td align="right"><input type="checkbox" name="checkbox" id="checkbox" />
+      <label for="checkbox"></label></td>
+      <td>&nbsp;</td>
+      <td>Reservar vaga no ônibus</td>
+    </tr>
+    <tr>
+      <td align="right">&nbsp;</td>
+      <td>&nbsp;</td>
+      <td align="right" valign="middle"><input type="submit" name="button" id="button" value="Cadastrar" /></td>
+    </tr>
+  </table>
+</form>
+</body>
+</html>
---
+++ recebe_form.php
@@ -0,0 +1,126 @@
+﻿<?php
+?> 
+<!doctype html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+</head>
+<body> 
+    
+<?php
+//dados do banco
+$hostname = '186.202.152.51';
+$user = 'unasp43';
+$pass = 'un@sp43';
+$dbase = 'unasp43';
+$con = null;
+
+
+//varias que recebem os dados do formulÃ¡rio
+$nome = $_POST['nome'] ? $_POST['nome'] : "";
+$sobrenome = $_POST['sobrenome'] ? $_POST['sobrenome'] : "";
+$telefone = (isset($_POST['telefone'])) ? $_POST['telefone'] : "";
+$email = (isset($_POST['email'])) ? $_POST['email'] : "";
+$oficina = (isset($_POST['oficina'])) ? $_POST['oficina'] : "";
+$ra = (isset($_POST['ra'])) ? $_POST['ra'] : "";
+$semestre = (isset($_POST['semestre'])) ? $_POST['semestre'] : "";
+$curso = (isset($_POST['curso'])) ? $_POST['curso'] : "";
+
+//Abre a conexÃ£o com o banco 
+$con = mysql_connect($hostname, $user, $pass); // conexÃ£o
+mysql_select_db($dbase); // seleciona o banco
+//seta todos em utf8
+mysql_query("SET character_set_connection=utf8");
+mysql_query("SET character_set_client=utf8");
+mysql_query("SET character_set_results=utf8");
+
+//Obtem os dados da tabela oficina
+$pegaOficina = mysql_query("SELECT * FROM secone WHERE oficina = '$oficina'");
+$resultOficina = mysql_num_rows($pegaOficina);
+//Obtem os dados da tabela email
+$pegaEmail = mysql_query("SELECT * FROM secone WHERE email = '$email'");
+// pega um resultado dos dados do email
+$resultEmail = mysql_num_rows($pegaEmail);
+//variável que recebe o post_ra caso ele exista
+$resultRA = null;
+
+//caso exista o post_ra $resultRa recebe
+if (!empty($ra)) {
+    $pegaRa = mysql_query("SELECT * FROM secone WHERE ra = '$ra'");
+    $resultRA = mysql_num_rows($pegaRa);
+}
+
+
+if($resultEmail > 0){?>
+     <script>
+        alert("Email já cadastrado nessa oficina");
+        location.href="http://www.unasp-ec.com/secone/";
+    </script>
+<?php
+}else if ($resultOficina == 55) { ?>
+    <script>
+        alert("Já foram preenchidas o numero de vagas para essa oficina");
+        location.href="http://www.unasp-ec.com/secone/";
+    </script>
+
+<?php
+}
+
+//verifica se o RA existe no sistema
+else if ($resultRA > 0) {
+    ?>
+       <script>
+        alert("RA já cadastrado nessa oficina");
+        location.href="http://www.unasp-ec.com/secone/";
+    </script>
+    <?php
+} else {
+    
+//executa a query no bancoI 
+   $sql = "INSERT INTO secone (id,nome,sobrenome,telefone,email,oficina,ra,curso,semestre) VALUES (NULL,'" . $nome . "','" . $sobrenome . "','" . $telefone . "','" . $email . "','" . $oficina . "','" . $ra . "','" . $curso . "','" . $semestre . "')";
+    $db = mysql_query($sql);
+    //executa a query no banco
+    if ($db) {
+        $from = 'noreply@unasp-ec.com';
+        $to = "wanderley.gazeta@unasp.edu.br";
+        $headers = "From: SECONE <noreply@unasp-ec.com>\r\n";
+        $headers .= "MIME-Version: 1.0\n";
+        $headers .= "Content-type: text/html; charset=ISO-8859-1\r\n";
+        $headers .= sprintf('Return-Path: %s%s', $from, PHP_EOL);
+        $headers .= sprintf('Reply-To: %s%s', $from, PHP_EOL);
+        $headers .= sprintf('X-Priority: %d%s', 3, PHP_EOL);
+        $headers .= sprintf('X-Mailer: PHP/%s%s', phpversion(), PHP_EOL);
+        $headers .= sprintf('Disposition-Notification-To: %s%s', $from, PHP_EOL);
+        $subject = utf8_decode('Cadastro Secone ' . $assunto);
+        $mensagem = utf8_decode('Olá¡ ' . $nome . '! Seu Cadastro foi efetuado com sucesso');
+        $assunto = 'II SECONE';
+        $mensagem = "O cadastro de <b>".$nome."</b> foi realizado com sucesso!</br>";
+		$mensagem .= "SEUS DADOS:</br>";
+		$mensagem .= "NOME: ".$nome."</br>";
+		$mensagem .= "TELEFONE:".$telefone."</br>";
+		$mensagem .= "EMAIL: ".$email."</br>";
+		$mensagem .= "OFICINA: ".$oficina."</br>";
+		$mensagem .= "RA: ".$ra."</br>";
+		$mensagem .= "CURSO: ".$curso."</br>";
+		$mensagem .= "SEMESTRE: ".$semestre."</br>";
+		$mensagem .= "Para visualizar todas as inscrições acesse <a href='http://www.unasp-ec.com/secone/inscritos.php'>AQUI</a></br>";
+
+        if (mail($to, $subject, $mensagem, $headers)) {
+		?>
+            <script>
+				alert("Cadastro realizado com sucesso");
+				location.href="http://www.unasp-ec.com/secone/";
+			</script>
+	<?php
+        } else {
+				echo 'EMAIL FALHOU';
+        }
+        //Encerra a conexÃ£o
+        mysql_close();
+    } else {
+        echo"ERRO NO CADASTRO" . mysql_errno();
+    }
+}
+?>
+</body>
+</html>
